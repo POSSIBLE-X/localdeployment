84938515b88296edb31a33835a5155b06e007576
M-Busk <125284318+M-Busk@users.noreply.github.com>
2024-09-23 17:03:11 +0200

    initial postgres integration

---
+++ initial_data/postgres/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE edcconsumer;
+GRANT ALL PRIVILEGES ON DATABASE edcconsumer TO postgres;
+
+CREATE DATABASE edcprovider;
+GRANT ALL PRIVILEGES ON DATABASE edcprovider TO postgres;
+
+CREATE DATABASE possibleportal;
+GRANT ALL PRIVILEGES ON DATABASE possibleportal TO postgres;
