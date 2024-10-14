70c0610bab1ee0e7e427f23fab0d0a103ccdcccf
M-Busk <125284318+M-Busk@users.noreply.github.com>
2024-10-14 09:24:28 +0200

    include did-web-service in docker stack

--- initial_data/postgres/init.sql
+++ initial_data/postgres/init.sql
@@ -6,3 +6,6 @@ GRANT ALL PRIVILEGES ON DATABASE edcprovider TO postgres;
 
 CREATE DATABASE possibleportal;
 GRANT ALL PRIVILEGES ON DATABASE possibleportal TO postgres;
+
+CREATE DATABASE didwebservice;
+GRANT ALL PRIVILEGES ON DATABASE didwebservice TO postgres;
