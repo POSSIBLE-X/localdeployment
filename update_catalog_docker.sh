91c23196e0dfe4627c9933b84f289850de8ad597
M-Busk <125284318+M-Busk@users.noreply.github.com>
2025-01-28 10:46:41 +0100

    simplify piveau deployment with new github mirrors

--- update_catalog_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker --config ./secrets/repo-docker-config pull dockerhub.fokus.fraunhofer.de:5000/piveau/hub/piveau-hub-repo:latest
-docker --config ./secrets/search-docker-config pull dockerhub.fokus.fraunhofer.de:5000/piveau/hub/piveau-hub-search:latest
-docker --config ./secrets/normalization-docker-config pull dockerhub.fokus.fraunhofer.de:5000/possible/normalization-urdna2015:latest
